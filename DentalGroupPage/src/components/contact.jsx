2ae5e1dec2b05d3a3537953edc6fd0f857b0bae7
mangel119 <miguel.sanabria@uao.edu.co>
2021-11-15 19:40:00 -0500

    footer

--- DentalGroupPage/src/components/contact.jsx
+++ DentalGroupPage/src/components/contact.jsx
@@ -150,10 +150,7 @@ export const Contact = (props) => {
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
+            &copy; 2021 Santiago Ayala. Juan José Meléndez. Miguel Angel Sanabria
           </p>
         </div>
       </div>
